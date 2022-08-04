86c32151e6bfc441ba2f64e3b0cb80c961fc5ce9
caio <alcarriacaio@gmail.com>
2022-08-03 21:55:18 -0300

    versão final2

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,23 @@
 function gerarCards(arr) {
     let ProdutosHTML = document.querySelector('.containerListaProdutos ul')
     ProdutosHTML.innerHTML=""
+   
     arr.forEach((elem) => {
-        const {img, nome, secao, preco} = elem
+        const {img, nome, secao, preco, componentes} = elem
         ProdutosHTML.insertAdjacentHTML("afterbegin", `
             <li>
                 <img src="${img}" alt="Imagem produto">
                 <h3>${nome}</h3>
+                <p>${componentes.join(', ')}</p>
                 <span>${secao}</span>
-                <p>R$ ${ preco }.00</p>
-                <button class="addCart"> Comprar </button>
+                <div class="addCartDiv">
+                    <p>R$ ${ preco }.00</p>
+                    <button class="addCart"> Comprar </button>
+                </div>
             </li>`)
     });
-    
+
+    addCart(document.querySelectorAll('.containerListaProdutos ul li'))
 }
 
 function addCards() {
@@ -63,3 +68,62 @@ function buscar() {
 }
 buscar()
 
+function gerarCart(elem) {
+    listCart = document.querySelector('.listOfProducts')
+        const {img, nome, secao, preco} = elem
+        listCart.insertAdjacentHTML("afterbegin", `
+                <div class="cartProduct">
+                    <div class="containerCart">
+                        <img class="itemPicture" src="${img}" alt="">
+                        <div class="info">
+                            <h3 class="title">${nome}</h3>
+                            <p class="secao">${secao}</p>
+                            <strong class="price" id= "${preco}">R$ ${preco},00</strong>
+                        </div>
+                    </div>
+                    <img src="./src/img/trash.jpg" alt="" class="trash">
+                </div>
+        `)
+    removeCart(document.querySelectorAll('.trash'))
+    quantidadeAndTotal(document.querySelectorAll('.price'))
+    
+}
+
+function addCart(params) {
+    let butoonAddCart= params //chamar como parametro da função lá em cima
+    butoonAddCart.forEach(butoonAddCart => butoonAddCart.addEventListener('click',function () {
+                item = butoonAddCart.children[1].textContent
+                produtos.forEach(elem => {
+                    const {nome}=elem
+                    if(nome == item){
+                        gerarCart(elem)
+                    }
+                });
+    })
+        
+    );
+}
+
+addCart()
+
+function removeCart(butoonRemoveCart) {
+    butoonRemoveCart.forEach(butoonRemoveCart => butoonRemoveCart.addEventListener('click', function () {
+        butoonRemoveCart.parentElement.remove()
+        quantidadeAndTotal(document.querySelectorAll('.price'))
+    }))
+    
+}
+removeCart()
+
+function quantidadeAndTotal(itens) {
+    document.querySelector('.quantidade strong').innerHTML= `${itens.length}`
+    let total=0
+    itens.forEach(elem => {
+        x = parseInt(elem.attributes.id.value)
+        total=total+x
+    });
+    document.querySelector('.totalPrice p').innerHTML= `R$ ${total},00`
+}
+
+
+   
